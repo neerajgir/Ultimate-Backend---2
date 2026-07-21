ba69bd1cfb3a68006793652a6ae872e6745dcd5a
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-21 15:30:48 +0500

    Establish database connection and add user model file

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import connectDB from "./config/db.js"
 dotenv.config()
 
 const app =  express()
@@ -11,5 +12,6 @@ app.get("/", (req,res)=>{
 const port = process.env.PORT || 8000
 
 app.listen(port, ()=>{
+    connectDB()
     console.log(`Server is running on ${port}`);
 })
