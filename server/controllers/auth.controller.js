7610d0bcb3b83d2876a026e9830459358f3d68d2
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-22 00:43:21 +0500

    Implement user signup functionality with token generation and cookie handling

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,3 +1,44 @@
+import generateToken from "../config/token.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+
+
 const signUp = async (req, res) => {
-    
+    try {
+        const {firstName, lastName, email, password, userName} = req.body
+        if(!firstName || !lastName || !email || !password || !userName){
+            return res.status(400).json({message: "Fill All Details.."})
+        }
+        let existUser = await User.findOne({email})
+        if(existUser){
+            return res.status(400).json({message: "User already exist"})
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = await User.create({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword,
+            userName
+        })
+
+        const token = generateToken(user._id)
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENVIRONMENT == "production",
+            sameSite: "strict",
+            maxAge: 7*24*60*60*1000
+        })
+
+        return res.status(201).json({user: {
+            firstName,
+            lastName,
+            email,
+            userName
+        }})
+    } catch (error) {
+        return res.status(500).json({Message: error})
+    }
 } 
+
+export {signUp}
--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from "./config/db.js"
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config()
 
 const app =  express()
-
-app.get("/", (req,res)=>{
-    res.send("hello")
-})
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api", authRouter)
 
 const port = process.env.PORT || 8000
 
