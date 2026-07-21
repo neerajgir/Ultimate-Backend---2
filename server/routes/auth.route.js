ec15582139a628578c736a5d4a723b23700d9497
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-21 15:56:43 +0500

    Add signup route and controller for user registration

---
+++ server/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express, {Router} from "express"
+import {signUp} from "../controllers/auth.controller.js"
+const authRouter = express(Router());
+
+authRouter.post("/signup", signUp)
+
+
+
+export default authRouter
