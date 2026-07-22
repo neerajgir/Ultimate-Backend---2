28986c2da51abb0c1a663745c1604b31a154d7ac
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-22 16:21:11 +0500

    create Register and login page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Route, Routes} from 'react-router-dom'
-import SignUp from './pages/signup';
-import LogIn from './pages/LogIn';
+import {Route, Routes} from "react-router-dom"
+import LogIn from "./pages/LogIn"
+import Register from './pages/Register';
+
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/SignUp" element = {<SignUp/>}/>
-      <Route path="/LogIn" element = {<LogIn/>}/>
+      <Route path="/register" element={<Register/>}/>
+      <Route path="/login" element={<LogIn/>}/>
     </Routes>
   )
 }
--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const LogIn = () => {
   return (
-    <div>LogIn</div>
+    <div className='w-full h-screen bg-black flex justify-center items-center'>
+      <div className="w-[90%] max-w-[500px] h-[500px] bg-[#141f1f] rounded flex flex-col justify-center items-center">
+        <h1 className="text-white text-[20px] font-semibold">Log In</h1>
+        <form action=""></form>
+      </div>
+    </div>
   )
 }
 
