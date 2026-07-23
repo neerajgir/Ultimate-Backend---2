1adaf9bded46e378ad0f6e2c31d00bc6d979837d
Neeraj Gir <neerajgoswami871@gmail.com>
2026-07-23 15:55:37 +0500

    feat(server): add multer middleware for profile image uploads in signup

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express, {Router} from "express"
 import {signUp, logIn, logOut} from "../controllers/auth.controller.js"
+import { upload } from "../middlewares/multer.js";
 const authRouter = express(Router());
 
-authRouter.post("/signup", signUp)
+authRouter.post("/signup", upload.single("profileImage") , signUp)
 authRouter.post("/login", logIn)
 authRouter.post("/logout", logOut)
 
